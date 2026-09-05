fde93165bc5661da62893c21d15a31b2af0e2927
alexyeo <ya352110@gmail.com>
2026-09-05 20:58:32 +0800

    commit

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,11 +1,55 @@
+'use client';
+import { useState } from 'react';
+import './page.css'
 
-export default function Home() {
+export default async function Home() {
+  const [target, setTarget] = useState();
 
-  let hi = ["1","2","3","4","5"]
+
+  async function getData() {
+    const url = "http://localhost:3001/location/getall";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error(error.message);
+      return null;
+    }
+  }
+
+  const result = await getData();
+  const ids = result?.data ? Object.keys(result.data) : [];
+  console.log(result);
 
   return (
-    <div>
-      admin
+    <div className="content">
+      <div className="app-container">
+
+        <nav className="sidebar" id="sidebar-links">
+          <h1><strong>Report List</strong></h1>
+          <ul id="sidebar-list">
+            {ids.map((id) => (
+              <li key={id} onClick={() => (result.data[id])}>
+                {id}
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="Main-content">
+          <nav className="Content" id="main-content">
+            <h1><strong>Report content</strong></h1>
+            <h3>address: {result?.data && target ? result.data[target]?.address : "No data available"}</h3>
+            <p>data</p>
+          </nav>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
